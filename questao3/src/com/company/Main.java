1e78cdb90423e4cb359e231571d77f3473a7ab7d
Felype <felypeee@live.com>
2022-03-06 21:06:31 -0300

    Acrescimo feito

--- questao3/src/com/company/Main.java
+++ questao3/src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String leitura;
-        System.out.println("Digite sua frase para avaliação do seu sentimento, fofo(a)!");
+        System.out.println("Digite sua frase para avaliação do seu sentimento, :-) para feliz e :-( para triste");
         leitura = sc.nextLine();
         String leitura2 = leitura;
 
@@ -17,34 +17,37 @@ public class Main {
 
         // O indexOf()método retorna a posição da primeira ocorrência do(s) caractere(s) especificado(s) em uma string.
         //Foi colocado o emoticon para ser encontrado na frase e somado, caso haja ocorrência.
+        //Quando não é encontrado o emotion, o retorno é -1.
 
         i = leitura.indexOf(":-)");
-
-            while (i != -1){
-            divertidoCont+=1;
-            leitura = leitura.substring(i+3);
+        while (i != -1 && leitura.length() != 0) {
+            divertidoCont++;
+            //i+3 pois inicia no : e conta 3 caracteres com substring
+            leitura = leitura.substring(i + 3);
             i = leitura.indexOf(":-)");
         }
+        System.out.println("Encontramos " + divertidoCont + " emoticon divertido na frase!");
+
 
         i = leitura2.indexOf(":-(");
-
-        while (i != -1){
-            chateadoCont+=1;
-            leitura2 = leitura2.substring(i+3);
+        while (i != -1) {
+            chateadoCont++;
+            //i+3 pois inicia no : e conta 3 caracteres com substring
+            leitura2 = leitura2.substring(i + 3);
             i = leitura2.indexOf(":-(");
         }
+        System.out.println("Encontramos " + chateadoCont + " emoticon chateado na frase!\n");
 
         //Comparações para expressão do sentimento.
 
-        if(divertidoCont > chateadoCont){
-            System.out.println("Divertido");
+        if (divertidoCont > chateadoCont) {
+            System.out.println("Você está divertido(a)!");
+        } else if (chateadoCont > divertidoCont) {
+            System.out.println("Você está chateado(a)!");
+        } else if (divertidoCont == chateadoCont) {
+            System.out.println("Você está com um sentimento neutro!");
         }
 
-        else if(chateadoCont > divertidoCont){
-            System.out.println("Chateado");
-        }
-        else if(divertidoCont==chateadoCont){
-            System.out.println("Neutro");
-        }
     }
 }
+
